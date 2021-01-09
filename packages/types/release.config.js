6fed4ff11aaa6d3a6b6c343f53b87cda1b0922e6
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2021-01-09 21:32:47 +0100

    chore: add missing release config

---
+++ packages/types/release.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  branches: [
+    "master",
+    { name: "develop", channel: "beta", prerelease: "beta" },
+  ],
+  repositoryUrl: "https://github.com/amfa-team/space-service.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/exec",
+      {
+        publishCmd: "git checkout -- package.json",
+      },
+    ],
+    [
+      "semantic-release-slack-bot",
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+        markdownReleaseNotes: true,
+      },
+    ],
+  ],
+};
