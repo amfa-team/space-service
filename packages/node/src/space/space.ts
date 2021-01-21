b4c7627445f62a54bcc16698d4e38e59103184d4
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2021-01-21 12:28:23 +0100

    fix: get space

--- packages/node/src/space/space.ts
+++ packages/node/src/space/space.ts
@@ -7,7 +7,7 @@ export async function getSpace(
 ): Promise<ISpace | null> {
   const endpoint = process.env.SPACE_SERVICE_API_ENDPOINT;
 
-  const res = await fetch(`${endpoint}admin/ban/check`, {
+  const res = await fetch(`${endpoint}get`, {
     method: "POST",
     body: JSON.stringify({ slug, token }),
   });
@@ -15,6 +15,7 @@ export async function getSpace(
   const result = await res.json();
 
   if (!result.success) {
+    console.error(result);
     throw new Error("[space-service-node/getSpace]: unexpected error");
   }
 
