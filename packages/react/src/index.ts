df2df260a8ed3553686877efa4725f55acdc2738
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2021-02-02 23:17:30 +0100

    feat: support space hydratation

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,3 +2,4 @@ export { SpaceServiceSettings } from "./context/SpaceServiceSettings";
 export { SpaceList } from "./components/SpaceList";
 export { SpacePage } from "./components/SpacePage";
 export { AdminApp } from "./admin/AdminApp";
+export { getSpaces } from "./api/space/useSpaceList";
