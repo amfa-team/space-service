e4609aa18ed5d317ef3b1d5b6e58ad2a7290a717
moroine <moroine.bentefrit@gmail.com>
2021-02-20 15:05:29 +0100

    feat: remove admin code

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,4 @@
 export { SpaceServiceSettings } from "./context/SpaceServiceSettings";
 export { SpaceList } from "./components/SpaceList";
 export { SpacePage } from "./components/SpacePage";
-// export { AdminApp } from "./admin/AdminApp";
 export { getSpaces } from "./api/space/useSpaceList";
