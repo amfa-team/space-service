3b66d6569338ad2bf74e3151b820398bd42d9adb
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2021-02-21 18:19:25 +0100

    fix: build

--- packages/api/src/mongo/client.ts
+++ packages/api/src/mongo/client.ts
@@ -48,8 +48,8 @@ async function getClient(url: string): Promise<Mongoose> {
       useUnifiedTopology: true,
       connectTimeoutMS: 10_000,
       poolSize: 2, // Maintain up to 2 socket connections
-      maxPoolSize: process.env.MAX_POOL_SIZE ?? 5,
-      minPoolSize: process.env.MIN_POOL_SIZE ?? 1,
+      maxPoolSize: Number(process.env.MAX_POOL_SIZE ?? 5),
+      minPoolSize: Number(process.env.MIN_POOL_SIZE ?? 1),
       serverSelectionTimeoutMS: 5_000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 30_000, // Close sockets after 30 seconds of inactivity
       keepAlive: true,
