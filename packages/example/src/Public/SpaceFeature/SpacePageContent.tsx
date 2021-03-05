3880fe35d86b1c9cf4d9a14ca3464cfe6fca15c1
Moroine <moroine.bentefrit@gmail.com>
2021-03-05 17:50:54 +0100

    feat: add space details

--- packages/example/src/Public/SpaceFeature/SpacePageContent.tsx
+++ packages/example/src/Public/SpaceFeature/SpacePageContent.tsx
@@ -1,7 +1,7 @@
 import type { ISpace } from "@amfa-team/space-service-types";
 import React from "react";
 
-export function SpacePageContent({ space }: ISpace) {
+export function SpacePageContent({ space }: { space: ISpace }) {
   return (
     <div
       style={{
@@ -10,8 +10,16 @@ export function SpacePageContent({ space }: ISpace) {
         height: "300px",
       }}
     >
-      {space.name}({space._id})
+      <h2>
+        {space.name}({space._id})
+      </h2>
       {space.imageUrl && <img src={space.imageUrl} width={300} alt="space" />}
+      <h3>highlight: </h3>
+      <p>{space.highlight}</p>
+      <h3>description: </h3>
+      <div dangerouslySetInnerHTML={{ __html: space.description ?? "" }} />
+      <h3>tags: </h3>
+      <p>{space.tags?.join(", ")}</p>
     </div>
   );
 }
