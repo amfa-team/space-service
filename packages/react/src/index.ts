f02a5d41e2597f519a5652531a5b22964bbf5bbe
Moroine <moroine.bentefrit@gmail.com>
2021-04-02 19:41:59 +0200

    feat: add dictionary exports

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -5,3 +5,4 @@ export { getSpaces } from "./api/space/useSpaceList";
 export { VoteList } from "./components/VoteList";
 export { PollList } from "./components/PollList";
 export { Vote } from "./components/Vote";
+export * from "./i18n/dictionary";
