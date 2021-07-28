fcd92b04ac3d44e3192499ca3943568c7e4a00da
Gethi <bigard.antoine@gmail.com>
2021-07-28 20:30:52 +0200

    feat: dummy api

--- packages/api/src/utils/env.ts
+++ packages/api/src/utils/env.ts
@@ -4,7 +4,7 @@ export function getEnv(key: string): string {
   const value = process.env[key];
 
   if (!value) {
-    throw new Error(`getEnv: missing env-var "${key}"`);
+    throw new Error(`get Env: missing env-var "${key}"`);
   }
 
   return value;
